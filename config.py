fbe2f8fbe73bea2ef6e8db3571377dc7ab468bd5
Rami Sbahi <sbahirami@gmail.com>
2020-10-08 22:36:13 -0400

    elephantsql

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/beers'
+SQLALCHEMY_DATABASE_URI = 'postgres://cpjgxaem:V32873N19sOPHDFDbBhNj5RZsnL6weqA@rajje.db.elephantsql.com:5432/cpjgxaem'
 SQLALCHEMY_ECHO = True
 DEBUG = True
---
+++ db-beers/create_test.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Items(product_id INTEGER PRIMARY KEY, seller_username VARCHAR(20) NOT NULL, item_name VARCHAR(20))
