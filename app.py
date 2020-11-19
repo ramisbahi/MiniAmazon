3bca35f564e9a70410f70dd08b51c5a3bb470695
Rami Sbahi <sbahirami@gmail.com>
2020-11-18 22:59:14 -0500

    can't edit others items

--- app.py
+++ app.py
@@ -308,28 +308,32 @@ def delete_item(product_id):
 @app.route('/edit-item/<product_id>', methods=['GET', 'POST'])
 @login_required
 def edit_item(product_id):
-    item = db.session.query(models.Items).filter(models.Items.product_id == product_id).one()
-    form = forms.ItemEditFormFactory.form(item)
-    if form.validate_on_submit():
-        form.errors.pop('database', None)
+    try:
+        item = db.session.query(models.Items).filter(models.Items.product_id == product_id).filter(models.Items.seller_username == current_user.username).one()
+        form = forms.ItemEditFormFactory.form(item)
+        if form.validate_on_submit():
+            form.errors.pop('database', None)
 
-        if (request.files['image']):
-            image = request.files['image']
-            apiUrl = 'https://api.imgur.com/3/image'
-            b64_image = base64.standard_b64encode(image.read())
-            params = {'image' : b64_image}
-            headers = {'Authorization' : 'Client-ID 12aa250c79dba8d'}
-            #client_id = '12aa250c79dba8d'
-            #client_secret = '0e132c4d82850eda1d2a172903f5a85bcea10a0b'
-            response = requests.post(apiUrl, headers=headers, data=params)
-            result = json.loads(response.text)
-            edit_posting = models.Items()
-            edit_posting.image = result['data']['link']
-            models.Items.edit(product_id, current_user.username, form.category.data, form.condition.data, form.item_name.data, form.price.data, form.quantity.data, edit_posting.image, form.description.data)
-        else:
-            models.Items.edit(product_id, current_user.username, form.category.data, form.condition.data, form.item_name.data, form.price.data, form.quantity.data, item.image, form.description.data)
+            if (request.files['image']):
+                image = request.files['image']
+                apiUrl = 'https://api.imgur.com/3/image'
+                b64_image = base64.standard_b64encode(image.read())
+                params = {'image' : b64_image}
+                headers = {'Authorization' : 'Client-ID 12aa250c79dba8d'}
+                #client_id = '12aa250c79dba8d'
+                #client_secret = '0e132c4d82850eda1d2a172903f5a85bcea10a0b'
+                response = requests.post(apiUrl, headers=headers, data=params)
+                result = json.loads(response.text)
+                edit_posting = models.Items()
+                edit_posting.image = result['data']['link']
+                models.Items.edit(product_id, current_user.username, form.category.data, form.condition.data, form.item_name.data, form.price.data, form.quantity.data, edit_posting.image, form.description.data)
+            else:
+                models.Items.edit(product_id, current_user.username, form.category.data, form.condition.data, form.item_name.data, form.price.data, form.quantity.data, item.image, form.description.data)
 
-        flash('Item been modified successfully')
+            flash('Item been modified successfully')
+            return redirect(url_for('sales_history'))
+    except:
+        flash('You are not selling this item or this is not a valid item.')
         return redirect(url_for('sales_history'))
     return render_template('edit-item.html', item=item, form=form)
 
